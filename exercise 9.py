aedbadef89c80a20e53f2967fdd9761a6295337d
fathima hanna <hannahfati123@gmail.com>
2024-11-25 09:29:39 +0400

    .

---
+++ exercise 9.py
@@ -0,0 +1,13 @@
+#exercise9
+
+#first step 
+def hello():
+    print("Hello")
+
+   # Main function to call hello()
+def main():
+    hello() 
+
+    #last step
+    if __name__ == "__main__": 
+        main()
